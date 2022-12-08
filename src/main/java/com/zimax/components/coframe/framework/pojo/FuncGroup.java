e9b9421c9988fd319e0f1d18bfd456b775872909
sushangwen <157297167@qq.com>
2022-12-08 11:53:44 +0800

    实现权限功能

---
+++ src/main/java/com/zimax/components/coframe/framework/pojo/FuncGroup.java
@@ -0,0 +1,69 @@
+package com.zimax.components.coframe.framework.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+/**
+ * @Author 施林丰
+ * @Date:2022/12/7 11:56
+ * @Description
+ */
+@Data
+public class FuncGroup {
+    /**
+     * 功能组编号
+     */
+    @TableId(type = IdType.AUTO)
+    private int funcGroupId;
+
+    /**
+     * 应用编号
+     */
+    private int appId;
+
+    /**
+     * 功能组名称
+     */
+    private String funcGroupName;
+
+    /**
+     * 父功能组编号
+     */
+    private int parentFuncGroupId;
+
+    /**
+     * 功能组层次
+     */
+    private int groupLevel;
+
+    /**
+     * 功能组序号
+     */
+    private String funcGroupSeq;
+
+    /**
+     * 显示顺序
+     */
+    private int displayOrder;
+
+    /**
+     * 是否为叶子
+     */
+    private char isLeaf;
+
+    /**
+     * 叶子节点数
+     */
+    private int subCount;
+
+    /**
+     * 应用信息
+     */
+    private String appInfo;
+
+    /**
+     * 租户信息
+     */
+    private String tenantId;
+}
---
+++ src/main/java/com/zimax/components/coframe/framework/pojo/FuncResource.java
@@ -0,0 +1,54 @@
+package com.zimax.components.coframe.framework.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+/**
+ * @Author 施林丰
+ * @Date:2022/12/7 11:57
+ * @Description
+ */
+@Data
+public class FuncResource {
+    /**
+     * 资源编号
+     */
+    @TableId(type = IdType.AUTO)
+    private int resId;
+
+    /**
+     * 功能编码
+     */
+    private String funcCode;
+
+    /**
+     * 资源类型
+     */
+    private String resType;
+
+    /**
+     * 资源路径
+     */
+    private String resPath;
+
+    /**
+     * 所属构件包
+     */
+    private String comPackName;
+
+    /**
+     * 资源名称
+     */
+    private String resName;
+
+    /**
+     * 应用信息
+     */
+    private String appInfo;
+
+    /**
+     * 租户信息
+     */
+    private String tenantId;
+}
