b5eab30b4848ce9e418c2ba9742b907a7ba00922
linjunjie <monkeylin2000@163.com>
2022-12-09 14:37:34 +0800

    预警事件的CRUD实现

--- src/main/java/com/zimax/mcrs/warn/controller/AlarmEventController.java
+++ src/main/java/com/zimax/mcrs/warn/controller/AlarmEventController.java
@@ -8,27 +8,19 @@ import com.zimax.mcrs.warn.service.AlarmEventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * 预警事件管理
  * @author 林俊杰
  * @date 2022/11/29
  */
 @RestController
-@RequestMapping("/alarmEvent")
+@RequestMapping("/warn")
 public class AlarmEventController {
 
     @Autowired
     private AlarmEventService alarmEventService;
-
-    /**
-     * 初始化查询
-     * @param alarmEventId 告警事件编号
-     * @return 告警事件
-     */
-    @RequestMapping("/find/{alarmEventId}")
-    public Result<?> queryAll(@PathVariable("alarmEventId") int alarmEventId) {
-        return Result.success();
-    }
 
     /**
      * 条件查询预警事件
@@ -42,17 +34,19 @@ public class AlarmEventController {
      * @param page 页码
      * @return 角色列表
      */
-    @GetMapping("/query")
-    public Result<?> query(@RequestParam int alarmEventId, @RequestParam String alarmEventTitle,@RequestParam String alarmLevel, @RequestParam String alarmCategory, @RequestParam String alarmType, @RequestParam String makeFormPeople, @RequestParam int limit, @RequestParam int page) {
-        return Result.success();
+    @GetMapping("/alarmEvent/query")
+    public Result<?> query(Integer alarmEventId, String alarmEventTitle, String alarmLevel, String alarmCategory,  String alarmType, String makeFormPeople, String limit,  String page, String order, String field) {
+        List alarmEvents = alarmEventService.queryAll(page,limit,alarmEventId,alarmEventTitle,alarmLevel,alarmCategory,alarmType,makeFormPeople,order,field);
+        return Result.success(alarmEvents, alarmEventService.count(alarmEventId,alarmEventTitle));
     }
 
     /**
      * 添加告警事件
      * @param alarmEvent 预警事件
      */
-    @RequestMapping("/add")
+    @PostMapping("/alarmEvent/add")
     public Result<?> addAlarmRule(@RequestBody AlarmEvent alarmEvent){
+        alarmEventService.addAlarm(alarmEvent);
         return Result.success();
     }
 
@@ -60,8 +54,9 @@ public class AlarmEventController {
      * 删除告警事件
      * @param alarmEventId 预警事件编码
      */
-    @DeleteMapping("/delete/{alarmEventId}")
-    public Result<?> removeAlarmEvent(@PathVariable("alarmEventId")int alarmEventId) {
+    @DeleteMapping("/alarmEvent/delete/{alarmEventId}")
+    public Result<?> removeAlarmEvent(@PathVariable("alarmEventId")Integer alarmEventId) {
+        alarmEventService.removeAlarm(alarmEventId);
         return  Result.success();
     }
 
@@ -69,10 +64,21 @@ public class AlarmEventController {
      * 修改告警事件
      * @param alarmEvent 预警规则
      */
-    @PutMapping("/update")
+    @PostMapping("/alarmEvent/update")
     public Result<?> updateAlarmEvent(@RequestBody AlarmEvent alarmEvent) {
+        alarmEventService.updateAlarm(alarmEvent);
         return Result.success();
     }
+
+//    /**
+//     * 修改启停用
+//     * @param alarmEvent 预警规则
+//     */
+//    @PostMapping("/alarmEvent/update")
+//    public Result<?> enableAlarmEvent(@RequestBody AlarmEvent alarmEvent) {
+//        alarmEventService.enableAlarm(alarmEvent);
+//        return Result.success();
+//    }
 
 
 }
