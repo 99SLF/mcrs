0bc5c346ebea44da3faa8ee8f5e4544c7c8c907f
linjunjie <monkeylin2000@163.com>
2022-12-21 15:47:35 +0800

    设备交换日志的编码实现与前端页面绘制以及设备页面的优化

--- src/main/java/com/zimax/mcrs/log/service/DeviceExchangeLogService.java
+++ src/main/java/com/zimax/mcrs/log/service/DeviceExchangeLogService.java
@@ -1,12 +1,16 @@
 package com.zimax.mcrs.log.service;
 
+import com.zimax.mcrs.config.ChangeString;
 import com.zimax.mcrs.log.mapper.DeviceExchangeLogMapper;
 import com.zimax.mcrs.log.pojo.DeviceExchangeLog;
+import com.zimax.mcrs.log.pojo.DeviceExchangeLogVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 设备交换日志服务
@@ -19,38 +23,52 @@ public class DeviceExchangeLogService {
     @Autowired
     private DeviceExchangeLogMapper deviceExchangeLogMapper;
 
-    /**
-     * 设备交换日志定期删除
-     *
-     */
-    public void removeDeviceExchangeLog(){
-
-    }
 
     /**
-     * 设备交换日志删除
-     * @param deviceExchangeLogId
-     */
-    public void removeById(int deviceExchangeLogId){
-
-    }
-
-    /**
-     * 设备交换日志初始化查询
+     * 查询所有的设备交换日志
      * @return DeviceExchangeLog
      */
-    public List<DeviceExchangeLog> queryAll(){
-        return null;
+    public List<DeviceExchangeLogVo> queryDeviceExchangeLog(String limit, String page, String equipmentId, String equipmentContinuePort, String processName,String operator, String exchangeTime, String order, String field){
+        ChangeString changeString = new ChangeString();
+        Map<String,Object> map= new HashMap<>();
+        if(order==null){
+            map.put("order","desc");
+            map.put("field","exchange.exchange_time");
+        }else{
+            map.put("order",order);
+            map.put("field",changeString.camelUnderline(field));
+        }
+        if (limit != null) {
+            map.put("begin", Integer.parseInt(limit) * (Integer.parseInt(page) - 1));
+            map.put("limit", Integer.parseInt(limit));
+        }
+        map.put("equipmentId",equipmentId);
+        map.put("equipmentContinuePort",equipmentContinuePort);
+        map.put("processName",processName);
+        map.put("operator",operator);
+        map.put("exchangeTime",exchangeTime);
+        return deviceExchangeLogMapper.queryAll(map);
     }
 
     /**
-     * 设备交换日志条件查询
-     * @return DeviceExchangeLog
+     * 查询记录
      */
-    public DeviceExchangeLog  query(int deviceId){
-        return null;
+    public int count(String equipmentId) {
+        return deviceExchangeLogMapper.count(equipmentId);
+    }
+
+    /**
+     * 添加设备交换日志
+     */
+    public void addDeviceExchangeLog(DeviceExchangeLog deviceExchangeLog){
+        deviceExchangeLogMapper.addDeviceExchangeLog(deviceExchangeLog);
     }
 
 
-
+    /**
+     * 删除设备交换日志
+     */
+    public void removeDeviceExchangeLog(int deviceExchangeLogId){
+        deviceExchangeLogMapper.removeDeviceExchangeLog(deviceExchangeLogId);
+    }
 }
