0368e0195e22d8575cedfea29099706bef75fd31
linjunjie <monkeylin2000@163.com>
2023-01-04 17:50:57 +0800

    异常日志的实现以及前端页面

---
+++ src/main/java/com/zimax/mcrs/log/pojo/AbnLogVo.java
@@ -0,0 +1,80 @@
+package com.zimax.mcrs.log.pojo;
+
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 异常日志
+ *
+ * @author 林俊杰
+ * @date 2023/1/4
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("log_abn_log")
+public class AbnLogVo {
+
+    /**
+     * 异常日志Id
+     */
+    @TableId(type = IdType.AUTO)
+    private int abnLogId;
+
+    /**
+     * 异常日志编号
+     */
+    private String abnLogNum;
+
+    /**
+     * 日志类型
+     */
+    private String logType;
+
+    /**
+     * 设备资源号
+     */
+    private String equipmentId;
+
+    /**
+     * APPId
+     */
+    private String APPId;
+
+    /**
+     * 终端名称
+     */
+    private String deviceName;
+
+    /**
+     * 预警标题
+     */
+    private String abnTitle;
+
+    /**
+     * 预警类型
+     */
+    private String abnType;
+
+    /**
+     * 预警等级
+     */
+    private String abnLevel;
+
+    /**
+     * 预警内容
+     */
+    private String abnContent;
+
+    /**
+     *交互时间
+     */
+    private String exchangeTime;
+
+
+}
---
+++ src/main/java/com/zimax/mcrs/log/service/AbnLogService.java
@@ -0,0 +1,97 @@
+package com.zimax.mcrs.log.service;
+
+import com.zimax.mcrs.config.ChangeString;
+import com.zimax.mcrs.log.mapper.AbnLogMapper;
+import com.zimax.mcrs.log.mapper.InterfaceLogMapper;
+import com.zimax.mcrs.log.pojo.AbnLog;
+import com.zimax.mcrs.log.pojo.AbnLogVo;
+import com.zimax.mcrs.log.pojo.InterfaceLog;
+import com.zimax.mcrs.log.pojo.InterfaceLogVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 接口日志
+ * @author 林俊杰
+ * @date 2023/1/4
+ */
+@Service
+public class AbnLogService {
+
+    @Autowired
+    private AbnLogMapper abnLogMapper;
+
+
+    /**
+     * 查询所有异常日志信息
+     * @param limit
+     * @param page
+     * @param equipmentId
+     * @param deviceName
+     * @param abnType
+     * @param abnLevel
+     * @param exchangeTime
+     * @param order
+     * @param field
+     * @return
+     */
+    public List<AbnLogVo> queryAbnLog(String limit, String page, String equipmentId, String deviceName, String abnType, String abnLevel, String exchangeTime, String order, String field){
+        ChangeString changeString = new ChangeString();
+        Map<String,Object> map= new HashMap<>();
+        if(order==null){
+            map.put("order","desc");
+            map.put("field","abn.exchange_time");
+        }else{
+            map.put("order",order);
+            map.put("field",changeString.camelUnderline(field));
+        }
+        if (limit != null) {
+            map.put("begin", Integer.parseInt(limit) * (Integer.parseInt(page) - 1));
+            map.put("limit", Integer.parseInt(limit));
+        }
+        map.put("equipmentId",equipmentId);
+        map.put("deviceName",deviceName);
+        map.put("abnType",abnType);
+        map.put("abnLevel",abnLevel);
+        map.put("exchangeTime",exchangeTime);
+        return abnLogMapper.queryAll(map);
+    }
+
+    /**
+     * 查询记录
+     */
+    public int count(String equipmentId) {
+        return abnLogMapper.count(equipmentId);
+    }
+
+
+    /**
+     * 检测设备是否存在
+     *
+     * @param equipmentInt 设备资源号
+     */
+    public int  checkEquipment(int equipmentInt) {
+        return abnLogMapper.checkEquipment(equipmentInt);
+    }
+
+    /**
+     * 添加异常日志
+     * @param abnLog
+     */
+    public void addAbnLog(AbnLog abnLog) {
+        abnLogMapper.addAbnLog(abnLog);
+    }
+
+    /**
+     * 删除异常日志
+     */
+    public void removeAbnLog(int abnLogId) {
+        abnLogMapper.removeAbnLog(abnLogId);
+    }
+
+
+}
