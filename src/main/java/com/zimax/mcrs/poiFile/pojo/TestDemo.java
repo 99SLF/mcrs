e52da4a61cbc5362d4f982b2b4cc188e3dcd5472
wangguangyu <baimaomao622@163.com>
2023-02-20 16:33:41 +0800

    文件导入导出

---
+++ src/main/java/com/zimax/mcrs/poiFile/pojo/TestDemo.java
@@ -0,0 +1,11 @@
+package com.zimax.mcrs.poiFile.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TestDemo {
+}
